ed89ff002e0c88b95e4c40cc28fd6f4110e500e8
Norifumi Konishi <norifumi.konishi@valtes-innovations.co.jp>
2025-10-15 11:53:04 +0900

    不要ファイル削除

--- JavaScript/demo/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("submitBtn").addEventListener("click", () => {
-  const name = document.getElementById("nameInput").value;
-  document.getElementById("output").textContent = `こんにちは、${name}さん！`;
-});
